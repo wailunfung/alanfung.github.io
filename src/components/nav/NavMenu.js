9714185bc9f2590cf8ca19fb0280e09554a78c5e
Alan Fung <wailunfung@gmail.com>
2021-03-30 09:51:44 -0700

    hiding journel in nav

--- src/components/nav/NavMenu.js
+++ src/components/nav/NavMenu.js
@@ -16,7 +16,7 @@ const Menu = ({ isActive }) => {
         <li><Link className={navStyles.menuItem} activeClassName={navStyles.menuItemActive} to="/portfolio">PORTFOLIO</Link></li>
         <li><Link className={navStyles.menuItem} activeClassName={navStyles.menuItemActive} to="/about">ABOUT</Link></li>
         <li><Link className={navStyles.menuItem} activeClassName={navStyles.menuItemActive} to="/contact">CONTACT</Link></li>
-        <li><Link className={navStyles.menuItem} activeClassName={navStyles.menuItemActive} to="/journal">JOURNAL</Link></li>
+        {/* <li><Link className={navStyles.menuItem} activeClassName={navStyles.menuItemActive} to="/journal">JOURNAL</Link></li> */}
       </ul>
     </div>
     </React.Fragment>
